b02244eca333e41ad03d6a76be8f8e00523f1bae
Saad Sabahuddin <saadmdsabahuddin@gmail.com>
2025-10-06 21:28:57 +0530

    First Commit

---
+++ src/main/java/com/cicd/cicdintegration/CicdintegrationApplication.java
@@ -0,0 +1,12 @@
+package com.cicd.cicdintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdintegrationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CicdintegrationApplication.class, args);
+  }
+}
---
+++ src/test/java/com/cicd/cicdintegration/CicdintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.cicdintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
